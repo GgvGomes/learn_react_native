2344faa04f0fd387a73fe79b905d3a15f510a158
GgvGomes <ggvgabriel05@gmail.com>
2023-03-24 08:51:23 -0300

    Buttons primary

--- components/buttons/button.js
+++ components/buttons/button.js
@@ -1,10 +1,32 @@
 import { Pressable, StyleSheet, Text, View } from "react-native";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
-export function Button({label}){
-    return(
-        <View style={styles.buttonContainer}>
+export function Button({ label, theme }) {
+    if (theme === "primary") {
+        return (
+            <View
+                style={[styles.buttonContainer, { borderWidth: 4, borderColor: "#ffd33d", borderRadius: 18 }]}
+            >
+                <Pressable
+                    style={[styles.button, { backgroundColor: "#fff" }]}
+                    onPress={() => alert('You pressed a button.')}
+                >
+                    <FontAwesome
+                        name="picture-o"
+                        size={18}
+                        color="#25292e"
+                        style={styles.buttonIcon}
+                    />
+                    <Text style={[styles.buttonLabel, { color: "#25292e" }]}>{label}</Text>
+                </Pressable>
+            </View>
+        );
+    }
+    
+    return (
+        <View style={styles.buttonContainer} >
             <Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
-                <Text style={styles.butttonLabel}>{label}</Text>
+                <Text style={styles.buttonLabel}>{label}</Text>
             </Pressable>
         </View>
     )
@@ -17,14 +39,15 @@ const styles = StyleSheet.create({
         marginHorizontal: 20,
         alingItems: 'center',
         justifyContent: 'center',
-        padding: 3,
+        padding: 4,
     },
 
     button: {
         borderRadius: 10,
         width: '100%',
         height: '100%',
-        alingItems: 'center',
+
+        alignItems: 'center',
         justifyContent: 'center',
         flexDirection: 'row',
     },
@@ -33,7 +56,7 @@ const styles = StyleSheet.create({
         paddingRight: 8,
     },
 
-    butttonLabel: {
+    buttonLabel: {
         color: '#fff',
         fontSize: 16,
     },
